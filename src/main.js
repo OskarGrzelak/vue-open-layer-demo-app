4f74792d61555367daccb9a2b183442f1c1f318e
Oskar Grzelak <oskar.grzelak@mix.smallgis.pl>
2021-12-07 13:56:30 +0100

    add web vitals to project

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import BaseButton from './components/ui/BaseButton.vue'
 import BaseControl from './components/ui/BaseControl.vue'
 import './tailwind.css'
 
+import { getLCP, getFID, getCLS } from 'web-vitals'
+
 const app = createApp(App)
 app.component('base-input', BaseInput)
 app.component('base-button', BaseButton)
 app.component('base-control', BaseControl)
 app.mount('#app')
+
+getCLS(console.log)
+getFID(console.log)
+getLCP(console.log)
